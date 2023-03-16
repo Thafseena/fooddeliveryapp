2658e2ae9b1c835541a432878d7c85f1515fdec8
Thafseena <thafseenamkd121996@gmail.com>
2023-03-16 20:16:10 +0530

    created splash screen and added the routing to current created pages

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:fooddeliveryapp/pages/food/recommended_food_detail.dart';
 import 'package:fooddeliveryapp/pages/home/food_page_body.dart';
 import 'package:fooddeliveryapp/pages/home/main_food_page.dart';
 import 'package:fooddeliveryapp/pages/food/popular_food_detail.dart';
+import 'package:fooddeliveryapp/pages/splash/splash_page.dart';
 import 'package:fooddeliveryapp/routes/route_helper.dart';
 import 'package:get/get.dart';
 import 'helper/depentencies.dart' as dep;
@@ -23,16 +24,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Get.find<PopularProductController>().getPopularProductList();
-    Get.find<RecommededProductController>().getRecommendedProductList();
+    return GetBuilder<PopularProductController>(builder: (_){
+        return GetBuilder<RecommededProductController>(builder: (_){
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            initialRoute: RouteHelper.getSplashPage(),
+            getPages: RouteHelper.routes,
+          );
+        });
+    });
+  /*  /*  Get.find<PopularProductController>().getPopularProductList();
+    Get.find<RecommededProductController>().getRecommendedProductList(); */
     print("aaa");
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       //home: CartPage(),
-      home: MainFoodPage(),
-      initialRoute: RouteHelper.initial,
+     // home: MainFoodPage(),
+    // home: SplashScreen(),
+      initialRoute: RouteHelper.getSplashPage(),
       getPages: RouteHelper.routes,
-    );
+    ); */
   }
 }
--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -1,23 +1,27 @@
 import 'package:fooddeliveryapp/pages/cart/cart_page.dart';
 import 'package:fooddeliveryapp/pages/food/popular_food_detail.dart';
 import 'package:fooddeliveryapp/pages/food/recommended_food_detail.dart';
+import 'package:fooddeliveryapp/pages/home/home_page.dart';
 import 'package:fooddeliveryapp/pages/home/main_food_page.dart';
+import 'package:fooddeliveryapp/pages/splash/splash_page.dart';
 import 'package:get/get.dart';
 
 class RouteHelper{
- 
+   static const String splashPage="/splash-page";
    static const String initial="/"; 
    static const String popularFood="/popular-food";
    static const String recommendedFood="/recommended-food";
    static const String cartPage="/cart-page";
     
+    static String getSplashPage()=>'$splashPage';
     static String getInitial()=>'$initial';
     static String getPopularFood(int pageId,String page)=>'$popularFood?pageId=$pageId&page=$page';
     static String getRecommendedFood(int pageId1,String page1)=>'$recommendedFood?pageId1=$pageId1&page1=$page1';
     static String getCartPage()=>'$cartPage';
 
    static List<GetPage> routes=[
-   GetPage(name: initial, page: ()=>MainFoodPage()),
+    GetPage(name: splashPage, page: ()=>SplashScreen()),
+   GetPage(name: initial, page: ()=>HomePage()),
    GetPage(name: popularFood , page: (){
     var pageId=Get.parameters['pageId'];
     var page=Get.parameters['page'];
